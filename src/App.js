6677d56650591b9898573342fe309b2735db9c0b
jgomez002 <jdg8858@nyu.edu>
2022-10-18 09:24:23 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import Choice from "./pages/choice";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+  {
+    path: "/selectorpage",
+    element: <Choice/>,
+  }, 
+  {
+    path: "/home",
+    element: <Home/>,
+  }, 
+
+]);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <RouterProvider router={router} />
       </header>
     </div>
   );
